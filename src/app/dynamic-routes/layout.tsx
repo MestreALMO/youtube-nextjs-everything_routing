3297d2584fce400398b46790b08070ae66d5cafe
André Lusegardis <lusegardis@gmail.com>
2023-06-29 06:52:44 -0300

    better looking site

--- src/app/dynamic-routes/layout.tsx
+++ src/app/dynamic-routes/layout.tsx
@@ -7,7 +7,6 @@ export default function CommentDynamicRoutes({
     <section>
       <div className="flex flex-col justify-center items-center">
         <h1>Dynamic Routes</h1>
-        <hr className="w-[100%] my-2 border-black" />
         <h2>Comment what is your favorite treat?</h2>
       </div>
 
